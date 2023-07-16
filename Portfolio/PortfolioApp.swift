f2b5abf993174921e025ff587f1c2ac72f35c831
AbdelrahmanShehab <abdoshehab7@gmail.com>
2023-07-16 04:41:07 +0300

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Abdelrahman Shehab on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
